aa91221be8d512c2abb13a8ac6ddbf36758da232
Yuri Hoogeweg <yuri.hoogeweg@mosaic-online-systems.com>
2019-12-02 14:01:35 +0000

    add day 1 part 2

--- 1/1.py
+++ 1/1.py
@@ -2,9 +2,28 @@ import math
 
 input_file = open('input.txt', 'r')
 module_masses = input_file.readlines()
-fuel = 0
+
+#Part 1
+def calculate_fuel(mass):
+    return math.floor(int(mass) / 3) - 2
+
+total_fuel = 0
 
 for mass in module_masses:
-    fuel += math.floor(int(mass) / 3) - 2
+    total_fuel += calculate_fuel(mass)
 
-print(fuel)
+print(total_fuel)
+
+total_fuel = 0
+# Part 2
+for mass in module_masses:        
+    fuel = calculate_fuel(mass)
+    total_fuel += fuel
+
+    fuel_mass = calculate_fuel(fuel)
+
+    while fuel_mass > 0:
+        total_fuel += fuel_mass
+        fuel_mass = calculate_fuel(fuel_mass)
+
+print(total_fuel)
