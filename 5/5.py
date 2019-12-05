2323221e90d3fb553f737bc229bf2371f07cd617
Yuri Hoogeweg <yuri.hoogeweg@mosaic-online-systems.com>
2019-12-05 15:36:20 +0000

    Make day 5 even less awful but still pretty awful

--- 5/5.py
+++ 5/5.py
@@ -1,6 +1,18 @@
 input_file = open('input.txt', 'r')
 program_array = [int(instruction) for instruction in input_file.read().split(",")]
 
+opcode_increments = { 
+    1: 4,
+    2: 4, 
+    3: 2,
+    4: 2,
+    5: 0,
+    6: 0,
+    7: 4,
+    8: 4,
+    99: 0
+}
+
 def get_immediate_value(program, pointer):
     return program[pointer]
 
@@ -8,6 +20,7 @@ def get_position_value(program, pointer):
     return program[program[pointer]] 
 
 def get_mode(instruction, param_num):
+    # Param 3 is always immediate because 'Parameters that an instruction writes to will never be in immediate mode'
     if param_num == 3:
         return 1
 
@@ -20,8 +33,8 @@ def get_mode(instruction, param_num):
     
     return modes[param_num -1]    
 
-def get_parameter_value(program, first_instruction, instruction_pointer, param_num):
-    mode = get_mode(first_instruction, param_num)
+def get_parameter_value(program, instruction, instruction_pointer, param_num):
+    mode = get_mode(instruction, param_num)
     param_pointer = instruction_pointer + param_num
 
     return get_position_value(program, param_pointer) if mode == 0 else get_immediate_value(program, param_pointer)
@@ -30,35 +43,31 @@ def run_program(program, input):
     instruction_pointer = 0
 
     while instruction_pointer < len(program): 
-        first_instruction = program[instruction_pointer]
-        opcode = first_instruction % 100
+        instruction = program[instruction_pointer]
+        opcode = instruction % 100
         
         if opcode == 1:
-            program[program[instruction_pointer + 3]] = get_parameter_value(program, first_instruction, instruction_pointer, 1) + get_parameter_value(program, first_instruction, instruction_pointer, 2)
-            instruction_pointer += 4
+            program[program[instruction_pointer + 3]] = get_parameter_value(program, instruction, instruction_pointer, 1) + get_parameter_value(program, instruction, instruction_pointer, 2)            
         if opcode == 2:
-            program[program[instruction_pointer + 3]] = get_parameter_value(program, first_instruction, instruction_pointer, 1) * get_parameter_value(program, first_instruction, instruction_pointer, 2)
-            instruction_pointer += 4
+            program[program[instruction_pointer + 3]] = get_parameter_value(program, instruction, instruction_pointer, 1) * get_parameter_value(program, instruction, instruction_pointer, 2)            
         if opcode == 3:
-            program[program[instruction_pointer + 1]] = input
-            instruction_pointer += 2            
+            program[program[instruction_pointer + 1]] = input            
         if opcode == 4:
-            val = get_parameter_value(program, first_instruction, instruction_pointer, 1)
+            val = get_parameter_value(program, instruction, instruction_pointer, 1)
             if val != 0:
-                return val          
-            instruction_pointer += 2
+                return val            
         if opcode == 5:
-            instruction_pointer = get_parameter_value(program, first_instruction, instruction_pointer, 2) if get_parameter_value(program, first_instruction, instruction_pointer, 1) != 0 else instruction_pointer + 3
+            instruction_pointer = get_parameter_value(program, instruction, instruction_pointer, 2) if get_parameter_value(program, instruction, instruction_pointer, 1) != 0 else instruction_pointer + 3
         if opcode == 6:
-            instruction_pointer = get_parameter_value(program, first_instruction, instruction_pointer, 2) if get_parameter_value(program, first_instruction, instruction_pointer, 1) == 0 else instruction_pointer + 3
+            instruction_pointer = get_parameter_value(program, instruction, instruction_pointer, 2) if get_parameter_value(program, instruction, instruction_pointer, 1) == 0 else instruction_pointer + 3
         if opcode == 7:
-            program[program[instruction_pointer + 3]] = 1 if get_parameter_value(program, first_instruction, instruction_pointer, 1) < get_parameter_value(program, first_instruction, instruction_pointer, 2) else 0
-            instruction_pointer += 4
+            program[program[instruction_pointer + 3]] = 1 if get_parameter_value(program, instruction, instruction_pointer, 1) < get_parameter_value(program, instruction, instruction_pointer, 2) else 0            
         if opcode == 8:
-            program[program[instruction_pointer + 3]] = 1 if get_parameter_value(program, first_instruction, instruction_pointer, 1) == get_parameter_value(program, first_instruction, instruction_pointer, 2) else 0
-            instruction_pointer += 4
+            program[program[instruction_pointer + 3]] = 1 if get_parameter_value(program, instruction, instruction_pointer, 1) == get_parameter_value(program, instruction, instruction_pointer, 2) else 0            
         if opcode == 99:
             return
+
+        instruction_pointer += opcode_increments[opcode]
 
 # Part 1
 print('Part 1: ' + str(run_program(program_array.copy(), 1)))
